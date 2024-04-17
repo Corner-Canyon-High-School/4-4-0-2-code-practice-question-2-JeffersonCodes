6b938bfa87631814d2a9b9d1dea5db7382b15594
JeffersonCodes <162729933+JeffersonCodes@users.noreply.github.com>
2024-04-17 15:53:00 +0000

    SUBMIT

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+red = int(input("Red: "))
+green = int(input("Green: "))
+blue = int(input("Blue: "))
+if not(red >= 0 and red <= 255):
+    print("Red number is not correct")
+if not(green >= 0 and green <= 255):
+    print("Green number is not correct")
+if not(blue >= 0 and blue <= 255):
+    print("Blue number is not correct")
+if(red >= 0 and red <= 255 and green >= 0 and green <= 255 and blue >= 0 and blue <= 255):
+    print("No problems found!")
